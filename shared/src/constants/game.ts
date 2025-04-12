08310fd36c77a62c82dbf43901711895c7b1ef05
manishjangra1 <dev.jangramanish@gmail.com>
2025-04-12 15:42:27 +0530

    client: add game hooks

---
+++ shared/src/constants/game.ts
@@ -0,0 +1,9 @@
+export const GAME_WIDTH = 1280;
+export const GAME_HEIGHT = 720;
+export const TICK_RATE = 60;
+export const TICK_INTERVAL = 1000 / TICK_RATE;
+
+export const PLAYER_SPEED = 300;
+export const JUMP_FORCE = -600;
+export const GRAVITY = 1200;
+export const DRAG = 500;
